dd354542160b2d00e7aba86f7145097f1f00b208
Spablob <tiagonunespereira@gmail.com>
2023-08-07 10:37:18 +0800

    post-comments: unwind permit adjustments

--- contracts/packages/unwind-permit/UnwindPermit.sol
+++ contracts/packages/unwind-permit/UnwindPermit.sol
@@ -6,7 +6,22 @@ import "./ECDSA.sol";
 import "./EIP712.sol";
 import "./Counters.sol";
 
-contract UnwindPermit is EIP712 {
+interface UnwindPermitInterface {
+    function checkOrderPermit(
+        address owner,
+        uint256 orderID,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+// UnwindPermit was based on Openzeppelin ERC20Permit
+// link: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/7f028d69593342673492b0a0b1679e2a898cf1cf/contracts/token/ERC20/extensions/ERC20Permit.sol
+
+contract UnwindPermit is EIP712, UnwindPermitInterface {
     using Counters for Counters.Counter;
 
     mapping(address => Counters.Counter) private _nonces;
@@ -15,10 +30,25 @@ contract UnwindPermit is EIP712 {
     bytes32 private constant _PERMIT_TYPEHASH =
         keccak256("UnwindPermit(address owner,uint256 orderID,uint256 value,uint256 nonce,uint256 deadline)");
 
+    // Unwind permit keeper
+    address public keeper;
+
+    // Current OTC wrapper address that is allowed to call checkOrderPermit()
+    address public currentOTCWrapper;
+
     /**
      * @dev Initializes the {EIP712} domain separator using the `name` parameter, and setting `version` to `"1"`.
      */
-    constructor(string memory name) EIP712(name, "1") {}
+    constructor(string memory name, address _keeper) EIP712(name, "1") {
+        require(_keeper != address(0), "UnwindPermit: keeper can not be zero");
+        keeper = _keeper;
+    }
+
+    function setOTCWrapper(address _otcWrapper) external {
+        require(msg.sender == keeper, "UnwindPermit: caller is not the keeper");
+        require(_otcWrapper != address(0), "UnwindPermit: OTC wrapper address can not be zero");
+        currentOTCWrapper = _otcWrapper;
+    }
 
     function checkOrderPermit(
         address owner,
@@ -29,6 +59,7 @@ contract UnwindPermit is EIP712 {
         bytes32 r,
         bytes32 s
     ) external {
+        require(msg.sender == currentOTCWrapper, "UnwindPermit: caller is not OTC Wrapper");
         require(block.timestamp <= deadline, "UnwindPermit: expired deadline");
 
         bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, owner, orderID, value, _useNonce(owner), deadline));
